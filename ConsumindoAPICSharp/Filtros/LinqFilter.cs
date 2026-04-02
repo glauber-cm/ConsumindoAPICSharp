3dda5374468cb4d39935b29603c3642b8df11532
Glauber <glauber.charles2012@gmail.com>
2026-04-02 17:42:49 -0300

    Foi criado a pasta de filtro com as classes LinqFilter e LinkOrder.

---
+++ ConsumindoAPICSharp/Filtros/LinqFilter.cs
@@ -0,0 +1,51 @@
+﻿using ConsumindoAPICSharp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsumindoAPICSharp.Filtros
+{
+    internal class LinqFilter
+    {
+        public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+        {
+            var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+            foreach (var genero in todosOsGenerosMusicais)
+            {
+                Console.WriteLine($"- {genero}");
+            }
+        }
+
+        public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero)
+        {
+            var artistasPorGeneroMusical = musicas.Where(musica => musica.Genero!.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+            Console.WriteLine($"Exibindo artistas por gênero musical >>> {genero}");
+            foreach (var artista in artistasPorGeneroMusical)
+            {
+                Console.WriteLine($"- {artista}");
+            }
+        }
+
+        public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeDoArtista)
+        {
+            var musicasDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeDoArtista)).ToList();
+            Console.WriteLine(nomeDoArtista);
+            foreach (var musica in musicasDoArtista)
+            {
+                Console.WriteLine($"- {musica.Nome}");
+            }
+        }
+
+        public static void FiltrarMusicasPorTonalidade(List<Musica> musicas, string tonalidadeDigitada)
+        {
+            var musicasEmTonalidade = musicas.Where(musica => musica.Tonalidade.Equals(tonalidadeDigitada)).Select(musica => musica.Nome).ToList();
+            Console.WriteLine($"Musicas em {tonalidadeDigitada}");
+            foreach (var musica in musicasEmTonalidade)
+            {
+                Console.WriteLine($"- {musica}");
+            }
+        }
+
+    }
+}
---
+++ ConsumindoAPICSharp/Filtros/LinqOrder.cs
@@ -0,0 +1,20 @@
+﻿using ConsumindoAPICSharp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumindoAPICSharp.Filtros
+{
+    internal class LinqOrder
+    {
+        public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+        {
+            var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+            Console.WriteLine("Lista de artistas ordenados");
+            foreach (var artista in artistasOrdenados)
+            {
+                Console.WriteLine($"- {artista}");
+            }
+        }
+    }
+}
